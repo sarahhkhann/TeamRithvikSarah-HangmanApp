1e8f7a7c8b3fc71b2c7f7b7759884f8d4b57c11f
Sarah Khan <sarkhan@bu.edu>
2024-02-13 13:00:33 -0500

    added keyboard panel

--- app/src/main/java/com/example/teamrithviksarah_hangmanapp/MainActivity.kt
+++ app/src/main/java/com/example/teamrithviksarah_hangmanapp/MainActivity.kt
@@ -3,11 +3,22 @@ package com.example.teamrithviksarah_hangmanapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the FragmentManager
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+
+        // Example: Replace a fragment
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.selectWords, lettersfragment())
+        transaction.commit()
+
 
 
 

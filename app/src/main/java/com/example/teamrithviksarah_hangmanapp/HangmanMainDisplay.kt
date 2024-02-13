ae6def7a94edbb741800a86c57ad6be4d8fe81c6
rithvik <rithvik@rntech.org>
2024-02-13 17:02:10 -0500

    ViewModel working

--- app/src/main/java/com/example/teamrithviksarah_hangmanapp/HangmanMainDisplay.kt
+++ app/src/main/java/com/example/teamrithviksarah_hangmanapp/HangmanMainDisplay.kt
@@ -1,60 +1,56 @@
 package com.example.teamrithviksarah_hangmanapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HangmanMainDisplay.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HangmanMainDisplay : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    companion object {
+        fun newInstance() = HangmanMainDisplay()
+    }
+
+    private lateinit var viewModel: HangmanViewModel
+    // Update this list to include all the text views
+    val textViewIds = listOf(
+        R.id.letter1, R.id.letter2, R.id.letter3, R.id.letter4, R.id.letter5)
+
+    private val textViews = mutableListOf<TextView>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewModel = ViewModelProvider(requireActivity()).get(HangmanViewModel::class.java)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hangman_main_display, container, false)
+        val view = inflater.inflate(R.layout.fragment_hangman_main_display, container, false)
+
+        // Initialize TextViews
+        textViewIds.forEach { id ->
+            val textView = view.findViewById<TextView>(id)
+            textViews.add(textView)
+        }
+
+        Log.d("Hangman", "JELLO")
+
+        viewModel.currentLetter.observe(viewLifecycleOwner, Observer { letter ->
+
+            textViews[0].text = letter
+        })
+
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HangmanMainDisplay.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HangmanMainDisplay().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }

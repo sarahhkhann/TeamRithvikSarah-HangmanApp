5f6bc539ae904af5a2cf66fcc31639ade1044eb6
Sarah Khan <sarkhan@bu.edu>
2024-02-13 15:19:44 -0500

    testing changes

--- app/src/main/java/com/example/teamrithviksarah_hangmanapp/HangmanMainDisplay.kt
+++ app/src/main/java/com/example/teamrithviksarah_hangmanapp/HangmanMainDisplay.kt
@@ -21,6 +21,8 @@ class HangmanMainDisplay : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var ks: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
